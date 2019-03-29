6964b2837cc3cddfba1dd9d135fb3be8d3d92319
Donovancm <donovan.dcm@gmail.com>
2019-03-29 10:28:26 +0100

    Add project files.

---
+++ ItemItem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItemItem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
