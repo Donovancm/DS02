e33eb3bccdecf1681fd4f38303f50660c0198d40
Donovancm <donovan.dcm@gmail.com>
2019-03-29 13:39:52 +0100

    Matrixtabel done .

---
+++ ItemItem/FileReader.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItemItem
+{
+    class FileReader
+    {
+        public FileReader()
+        {
+
+        }
+        public static Dictionary<int, double[,]> GetData(double[,] data)
+        {
+            var dictionary = new Dictionary<int, double[,]>();
+            int rowLength = data.GetLength(0);
+            int colLenght = data.GetLength(1);
+            bool checkeNewRow = false;
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                int key = -1;
+                double productvalue = -1;
+                double userRating = -1;
+                checkeNewRow = false;
+
+                for (int j = 0; j < colLenght; j++)
+                {
+                    //Console.WriteLine(data[i, j]);
+                    if (!checkeNewRow)
+                    {
+                        key = int.Parse(data[i, j] + "");
+                        checkeNewRow = true;
+
+                    }
+
+                    else if (j == 1)
+                    {
+                        productvalue = data[i, j];
+                    }
+                    else if (j == 2)
+                    {
+                        userRating = data[i, j];
+                        var userdata = new double[,]
+                        {
+                            {productvalue,userRating}
+                        };
+                        if (dictionary.ContainsKey(key))
+                        {
+                            var data2 = new double[,] { };
+                            data2 = dictionary[key];
+                            int rowLengthdata2 = data2.GetLength(0) - 1;
+                            int colLenghtdata2 = 2;
+                            int rowLengthdata3 = rowLengthdata2 + 1;
+                            int newRow2Ddata3 = data2.GetLength(0) + 1;
+                            var data3 = new double[newRow2Ddata3, colLenghtdata2];
+
+                            for (int r = 0; r <= rowLengthdata2; r++)
+                            {
+                                for (int c = 0; c < colLenghtdata2; c++)
+                                {
+                                    data3[r, c] = data2[r, c];
+                                }
+
+                            }
+                            data3[rowLengthdata3, 0] = productvalue;
+                            data3[rowLengthdata3, 1] = userRating;
+                            dictionary[key] = data3;
+
+                        }
+                        else { dictionary.Add(key, userdata); }
+
+                    }
+
+                }
+            }
+            return dictionary;
+        }
+        public static double[] GetItemList(double[,] data)
+        {
+            var item = new List<double>();
+            for (int i = 0; i <= data.GetLength(0) - 1 ; i++)
+            {
+                for (int j = 0; j <= data.GetLength(1) -1; j++)
+                {
+                    if(!item.Contains(data[i,1]))
+                    {
+                        item.Add(data[i, 1]);
+                    }
+                    
+                }
+            }
+            double[] array = item.ToArray();
+            Array.Sort(array);
+            return array;
+        }
+    }
+}
---
+++ ItemItem/Matrices/CreateMatrix.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItemItem.Matrices
+{
+    class CreateMatrix
+    {
+        public static void CalculateAverageRating(double[,] matrix)
+        {
+            
+            for (int i = 0; i <= matrix.GetLength(0) - 1; i++)
+            {
+                double sumrating = 0;
+                double totalItems = 0;
+                for (int j = 2; j <= matrix.GetLength(1) - 1; j++)
+                {
+                    if (matrix[i,j] > 0)
+                    {
+                        totalItems++;
+                        sumrating += matrix[i, j];
+                    }
+                }
+                matrix[i, 1] = sumrating / totalItems;
+            }
+        }
+        public static double[,] Create(double[] itemList, Dictionary<int, double[,]> item)
+        {
+            var matrix = new double[item.Count, itemList.Length +2];
+            var index = -1;
+            foreach (var Useritem in item)
+            {
+                index++;
+                matrix[index, 0] = Useritem.Key;
+                var ratings = item[Useritem.Key];
+                for (int i = 0; i <= ratings.GetLength(0)-1; i++)
+                {
+                    for (int a = 0; a <= itemList.Length - 1; a++)
+                    {
+                       
+                            Console.Write(ratings.GetLength(1) - 1);
+                        if (ratings[i, 0] != itemList[a] && a == itemList.Length - 1)
+                        {
+                            matrix[index, a + 2] = 9999;
+                            // set null 
+                            break;
+                        }
+                        else if (ratings[i, 0] == itemList[a])
+                        {
+                            //set rating and calculate avg rating
+                            matrix[index, a + 2] = ratings[i, 1];
+                            break;
+                        }
+
+                    }
+                    
+                }
+
+            }
+            CalculateAverageRating(matrix);
+            return matrix;
+        }
+    }
+}
